a56eea051dade586a58d523f45e23d78ea55846f
Shirakami14 <jomuel0614@gmail.com>
2024-05-14 15:51:14 +0800

    make changes (removed unnecessary buttons and label)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,72 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnONE = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnONE
-            // 
-            this.btnONE.Location = new System.Drawing.Point(319, 247);
-            this.btnONE.Name = "btnONE";
-            this.btnONE.Size = new System.Drawing.Size(57, 23);
-            this.btnONE.TabIndex = 0;
-            this.btnONE.Text = "Shish";
-            this.btnONE.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(26, 247);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(147, 192);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Alejandro G Cash Mod";
-            // 
-            // button2
-            // 
-            this.button2.AutoSize = true;
-            this.button2.Location = new System.Drawing.Point(169, 44);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 26);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "push this beach";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(410, 282);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btnONE);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnONE;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,5 @@ namespace EZBanking
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
